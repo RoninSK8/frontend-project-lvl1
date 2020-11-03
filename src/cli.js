24aecaf62a6e0d65ffd9e2b53be7fe0ffd5dfa6e
Сергей Политаев <ronin@sonikelf.ru>
2020-11-03 08:29:52 +0300

    added changes to brain-games.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import promptly from 'promptly';
 
 async function askName() {
+  console.log('Welcome to the Brain Games!');
   const name = await promptly.prompt('May I have your name?');
   console.log(`Hello, ${name}!`);
 }
