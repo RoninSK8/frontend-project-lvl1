4b4baca65b0dd4bd8aaa8f5dff14f031bce4f8cc
Сергей Политаев <ronin@sonikelf.ru>
2020-11-03 08:43:47 +0300

    debug brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,30 +1,28 @@
 #!/usr/bin/env node
 import promptly from 'promptly';
-import askName from '../src/cli.js';
 
 async function isEvenGame() {
-  const isEven = (num) => {
-    return num % 2 === 0 ? 'yes' : 'no';
-  };
-  await askName();
+  const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+  console.log('Welcome to the Brain Games!');
+  const name = await promptly.prompt('May I have your name?');
+  console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let counter = 0;
   while (counter < 3) {
-    let randomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+    const randomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
     console.log(`Question: ${randomNumber}`);
+    // eslint-disable-next-line no-await-in-loop
     const answer = await promptly.prompt('Your answer: ');
     if (isEven(randomNumber) === answer) {
       console.log('Correct!');
       counter += 1;
-    }
-    else {
+    } else {
       counter = 0;
-      console.log(`${answer} is wrong answer ;(. Correct answer was '${isEven(randomNumber)}'.
-              Let's try again, Bill!`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(randomNumber)}'. \nLet's try again, ${name}!`);
     }
   }
   if (counter === 3) {
-    console.log('Congratulations, Sam!');
+    console.log(`Congratulations, ${name}!`);
   }
 }
 
