058e18a081df13576af5d2efdd6a1fee2720024e
Сергей Политаев <ronin@sonikelf.ru>
2020-11-12 05:25:43 +0300

    added changes to index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-undef */
+import promptly from 'promptly';
+
+async function game() {
+  console.log('Welcome to the Brain Games!');
+  const name = await promptly.prompt('May I have your name?');
+  console.log(`Hello, ${name}!`);
+  console.log(rules);
+  let scoreCount = 0;
+  const scoreToWin = 3;
+  while (scoreCount < scoreToWin) {
+    console.log(`Question: ${task}`);
+    const answer = await promptly.prompt('Your answer: ');
+    if (correctAnswer === +answer) {
+      console.log('Correct!');
+      scoreCount += 1;
+    } else {
+      scoreCount = 0;
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`);
+    }
+  }
+}
+export default game;
