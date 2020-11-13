6b673b768ea8307e62456d0fa33043ff408d0925
Сергей Политаев <ronin@sonikelf.ru>
2020-11-13 05:24:39 +0300

    added changes to brain-progression.js to run game

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,36 +1,5 @@
 #!/usr/bin/env node
-import promptly from 'promptly';
+import { rules, generateTaskAndCorrectAnswer } from '../games/progression.js';
+import game from '../src/index.js';
 
-async function isMissingNumber() {
-  console.log('Welcome to the Brain Games!');
-  const name = await promptly.prompt('May I have your name?');
-  console.log(`Hello, ${name}!`);
-  console.log('What number is missing in the progression?');
-  let counter = 0;
-  while (counter < 3) {
-    const numberOfItems = Math.floor(Math.random() * (10 - 5)) + 5;
-    const startingItem = Math.floor(Math.random() * (50 - 1)) + 1;
-    const sizeOfStep = Math.floor(Math.random() * (10 - 1)) + 1;
-    const indexOfSkippedItem = Math.floor(Math.random() * numberOfItems);
-    const resultArray = [];
-    for (let i = 1; i <= numberOfItems; i += 1) {
-      resultArray.push(startingItem + (sizeOfStep * i));
-    }
-    const rightAnswer = resultArray[indexOfSkippedItem];
-    resultArray[indexOfSkippedItem] = '..';
-    console.log(`Question: ${resultArray.join(' ')}`);
-    // eslint-disable-next-line no-await-in-loop
-    const answer = await promptly.prompt('Your answer: ');
-    if (rightAnswer === +answer) {
-      console.log('Correct!');
-      counter += 1;
-    } else {
-      counter = 0;
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. \nLet's try again, ${name}!`);
-    }
-    if (counter === 3) {
-      console.log(`Congratulations, ${name}!`);
-    }
-  }
-}
-isMissingNumber();
+game(rules, generateTaskAndCorrectAnswer);
