87ffaa501b38d5cc1ab8756245aef0bb4ea5a2e5
Сергей Политаев <ronin@sonikelf.ru>
2020-11-13 04:55:11 +0300

    added changes to brain-calc.js to run game

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,43 +1,5 @@
 #!/usr/bin/env node
-import promptly from 'promptly';
+import { rules, generateTaskAndCorrectAnswer } from '../games/calc.js';
+import game from '../src/index.js';
 
-async function calculationGame() {
-  console.log('Welcome to the Brain Games!');
-  const name = await promptly.prompt('May I have your name?');
-  console.log(`Hello, ${name}!`);
-  console.log('What is the result of the expression?');
-  let counter = 0;
-  while (counter < 3) {
-    const randomNumber1 = Math.floor(Math.random() * (100 - 1)) + 1;
-    const randomNumber2 = Math.floor(Math.random() * (100 - 1)) + 1;
-    const operators = '+-*';
-    // eslint-disable-next-line space-infix-ops
-    const randomOperator = operators[Math.floor(Math.random()*operators.length)];
-    let rightResult;
-    switch (randomOperator) {
-      case '+':
-        rightResult = randomNumber1 + randomNumber2;
-        break;
-      case '-':
-        rightResult = randomNumber1 - randomNumber2;
-        break;
-      default:
-        rightResult = randomNumber1 * randomNumber2;
-        break;
-    }
-    console.log(`Question: ${randomNumber1} ${randomOperator} ${randomNumber2}`);
-    // eslint-disable-next-line no-await-in-loop
-    const answer = await promptly.prompt('Your answer: ');
-    if (rightResult === +answer) {
-      console.log('Correct!');
-      counter += 1;
-    } else {
-      counter = 0;
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightResult}'. \nLet's try again, ${name}!`);
-    }
-    if (counter === 3) {
-      console.log(`Congratulations, ${name}!`);
-    }
-  }
-}
-calculationGame();
+game(rules, generateTaskAndCorrectAnswer);
