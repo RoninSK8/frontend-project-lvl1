4821d097f3acd268bd2cd45590011c16fef73ae0
Сергей Политаев <ronin@sonikelf.ru>
2020-11-13 04:15:07 +0300

    added changes to brain-even.js to run game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,29 +1,5 @@
 #!/usr/bin/env node
-import promptly from 'promptly';
+import { rules, generateTaskAndCorrectAnswer } from '../games/even.js';
+import game from '../src/index.js';
 
-async function isEvenGame() {
-  const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
-  console.log('Welcome to the Brain Games!');
-  const name = await promptly.prompt('May I have your name?');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let counter = 0;
-  while (counter < 3) {
-    const randomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
-    console.log(`Question: ${randomNumber}`);
-    // eslint-disable-next-line no-await-in-loop
-    const answer = await promptly.prompt('Your answer: ');
-    if (isEven(randomNumber) === answer) {
-      console.log('Correct!');
-      counter += 1;
-    } else {
-      counter = 0;
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(randomNumber)}'. \nLet's try again, ${name}!`);
-    }
-  }
-  if (counter === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
-}
-
-isEvenGame();
+game(rules, generateTaskAndCorrectAnswer);
