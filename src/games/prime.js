2b306d357455b8a605671824178e92f3a3242eb9
Сергей Политаев <ronin@sonikelf.ru>
2020-11-24 05:59:01 +0300

    refactor logic

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,18 +4,10 @@ import generateNumber from '../utils.js';
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num <= 1) {
-    return false;
+  for (let i = 2; i < num / 2; i += 1) {
+    if (num % i === 0) return false;
   }
-  if (num === 2 || num === 3) {
-    return true;
-  }
-  for (let i = 2; i <= num / 2; i += 1) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return true;
+  return num > 1;
 };
 
 const generateTaskAndCorrectAnswer = () => {
