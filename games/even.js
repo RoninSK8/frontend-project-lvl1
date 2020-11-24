3015b481752bd9b2266fb3384a1dfd86d4915a8f
Сергей Политаев <ronin@sonikelf.ru>
2020-11-24 04:17:32 +0300

    refactor even.js

--- games/even.js
+++ games/even.js
@@ -4,12 +4,9 @@ const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const generateTaskAndCorrectAnswer = () => {
-  const result = [];
   const task = generateNumber(1, 100);
-  result.push(task);
   const correctAnswer = isEven(task) ? 'yes' : 'no';
-  result.push(correctAnswer);
-  return result;
+  return [task, correctAnswer];
 };
 
 export { rules, generateTaskAndCorrectAnswer };
