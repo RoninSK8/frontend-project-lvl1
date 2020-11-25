83e807503da780756b45fef26a7035195198249a
Сергей Политаев <ronin@sonikelf.ru>
2020-11-25 03:09:10 +0300

    fixed isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,7 +4,7 @@ import generateNumber from '../utils.js';
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num / 2; i += 1) {
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) return false;
   }
   return num > 1;
